e8d35f76f1cf2659fb41c087cca32a6b219f90c1
Julien <Julienetie@gmail.com>
2016-07-01 20:39:38 +0100

    Added getOptionsViaDataset helper function

--- simple-accordion.js
+++ simple-accordion.js
@@ -56,156 +56,178 @@ window.test = document.getElementsByClassName('accordion')[0];
 
 
 (function(window, document, undefined) {
-window.sa = function(accordion, options){
-    var simpleAccordion = {},
-    $A = simpleAccordion;
-    $A.id = '';
-    $A.el = {};
-    $A.store = {};
-    $A.store.contentComputedHeights = {};
-    $A.accordion = '';
-    $A.sectionNodes = [];
-    $A.defaults = {};
+    window.sa = function(accordion, options) {
+        var simpleAccordion = {},
+            $A = simpleAccordion;
+        $A.id = '';
+        $A.el = {};
+        $A.store = {};
+        $A.store.contentComputedHeights = {};
+        $A.accordion = '';
+        $A.sectionNodes = [];
+        $A.defaults = {};
 
-    // http://addyosmani.com/polyfillthehtml5gaps/slides/#78
-    function fix(prop) {
-        var prefixes = ['Moz', 'Khtml', 'Webkit', 'O', 'ms'],
-            elem = document.createElement('div'),
-            upper = prop.charAt(0).toUpperCase() + prop.slice(1);
+        // http://addyosmani.com/polyfillthehtml5gaps/slides/#78
+        function fix(prop) {
+            var prefixes = ['Moz', 'Khtml', 'Webkit', 'O', 'ms'],
+                elem = document.createElement('div'),
+                upper = prop.charAt(0).toUpperCase() + prop.slice(1);
 
-        if (prop in elem.style)
-            return prop;
-
-        for (var len = prefixes.length; len--;) {
-            if ((prefixes[len] + upper) in elem.style)
-                return (prefixes[len] + upper);
-        }
-    }
-
-    function randomReference() {
-        return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-    }
-
-    simpleAccordion.init = function(accordion, options) {
-        var $A = simpleAccordion,
-            public = {},
-            uniqueID = '' + Math.random(Date.now());
-            instance = 0;
-        if (typeof accordion === 'string') {
-            accordion = document.querySelector(accordion);
-        } else if (accordion.nodeType !== 1) {
-            // console.error('nodeType is incorrect');
-        }
-        if (options.constructor !== {}.constructor) {
-            // console.error('Options should be an object literal');
-        }
-
-        $A.options = options;
-        $A.accordion = accordion;
-        $A.id = randomReference() + uniqueID.substr(uniqueID.length -5, uniqueID.length -1);
-        $A.setDefaults();
-        $A.getElements();
-        $A.setInitialState();
-        $A.bindEvents();
-
-        // console.info($A.id, $A.accordion)
-        public.destroy = $A.destroy;
-        return public;
-    }
-
-    simpleAccordion.setDefaults = function() {
-        this.defaults.event = this.options.event || 'click';
-        this.defaults.dimension = this.options.orientation === 'horizontal' ? 'width' : 'height';
-        this.defaults.contentOverflow = this.options.contentOverflow || 'hidden';
-        this.defaults.exposure = this.options.exposure || 0;
-        this.defaults.dynamicContent = !this.options.dynamicContent || true;
-    }
-
-    simpleAccordion.getElements = function() {
-        var el = this.el,
-            sectionNodes = this.sectionNodes,
-            options = this.options,
-            prefix = 'section-',
-            id;
-
-        sectionNodes = sectionNodes.slice.call(this.accordion.querySelectorAll(options.section));
-
-        sectionNodes.forEach(function(section, i) {
-            id = el[prefix + i] = {};
-            id.section = section;
-            id.switch = section.querySelector(options.switch);
-            id.indicator = section.querySelector(options.indicator);
-            id.content = section.querySelector(options.content);
-            id.contentBody = el[prefix + i].content.children[0];
-            id.close = el[prefix + i].contentBody.querySelector(options.close);
-        });
-
-        // simpleAccordion.setInitialState();
-        // simpleAccordion.bindEvents();
-    }
-
-    simpleAccordion.setInitialState = function() {
-        var el = this.el;
-        for (section in el) {
-            // Get contentBody element
-            var contentBody = el[section].contentBody;
-            // Hide contents overflow
-            el[section].content.style.overflow = this.defaults.contentOverflow;
-            // Set content sections to zero dimension
-            el[section].content.style[this.defaults.dimension] = this.defaults.exposure;
-            // Set content transition 
-            el[section].content.style[fix('transition')] = 'all 0.5s ease';
-            // Get computed dimension immediately if content body is not dynamic
-            if (this.defaults.dynamicContent) {
-                this.store.contentComputedHeights[section] = parseInt(window.getComputedStyle(contentBody, null).getPropertyValue(this.defaults.dimension), 10);
+            if (prop in elem.style) {
+                return prop;
             }
-        }
-    }
 
-    simpleAccordion.filterEvents = function(e) {console.log($A.accordion)
-            var el = $A.el,
-                target = e.target;
-            for (section in el) {
-                if (el[section].switch === target) {
-                    $A.toggleSection(el[section], section);
+            for (var len = prefixes.length; len--;) {
+                if ((prefixes[len] + upper) in elem.style) {
+                    return (prefixes[len] + upper);
                 }
             }
         }
 
-    simpleAccordion.bindEvents = function() {
-        var self = this;
-        // Add one event type per accordion element
-        this.accordion.addEventListener(this.defaults.event, simpleAccordion.filterEvents, false);
-    }
+        function getOptionsViaDataset(accordion){
+                data = accordion.getAttribute('data-simple-accordion'),
+                dataArray, optionsFromDataset = {};
 
-    simpleAccordion.toggleSection = function(section, sectionName) {
-
-        var contentBodyHeight = this.store.contentComputedHeights[sectionName];
-        // Get "Content" height
-        var contentHeight = parseInt(window.getComputedStyle(section.content, null).getPropertyValue(this.defaults.dimension), 10);
-
-        if (contentHeight) {
-            // Section content closed action
-            section.content.style.height = 0;
-        } else {
-            // Section content opened action
-            // Get computed dimension if content is dynamic
-            if (!this.defaults.dynamicContent) {
-                this.store.contentComputedHeights[sectionName] = parseInt(window.getComputedStyle(section.contentBody, null).getPropertyValue(this.defaults.dimension), 10);
+            if (!options && accordion.getAttribute('data-simple-accordion')) {
+                dataArray = data.replace(';', ',', -1).split(',');
+                
+                dataArray.map(function(pair) {
+                    return pair.split(':').map(function(part) {
+                        return part.replace(';', '').trim();
+                    });
+                }).forEach(function(pairAsArray){
+                    optionsFromDataset[pairAsArray[0]] = pairAsArray[1];
+                });
             }
-            section.content.style.height = contentBodyHeight + 'px';
+            return optionsFromDataset;
         }
-        console.log(contentHeight)
-    }
 
-    simpleAccordion.destroy = function() {
-        console.log(simpleAccordion.accordion, simpleAccordion.defaults.event)
-        simpleAccordion.accordion.removeEventListener(simpleAccordion.defaults.event, simpleAccordion.filterEvents);
-    }
+        function randomReference() {
+            return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+        }
 
-                            // var simpleAccordionINST = 
-                                // console.log()
-    // window.simpleAccordion = Object.assign({}, simpleAccordion).init;
-    return simpleAccordion.init(accordion, options);
-}
+        simpleAccordion.init = function(accordion, options) {
+            // var $A = simpleAccordion,
+            //     public = {},
+            //     uniqueID = '' + Math.random(Date.now());
+            // instance = 0;
+
+            // var isSelectorAString = typeof accordion === 'string',
+            //     isSelectorAnElement = accordion.nodeType === 1,
+            //     isOptionsAnObjectLiteral = options.constructor === {}.constructor;
+
+
+
+            // if (typeof accordion === 'string') {
+            //     accordion = document.querySelector(accordion);
+            // } else if (accordion.nodeType !== 1) {
+            //     // console.error('nodeType is incorrect');
+            // }
+            // if (options.constructor !== {}.constructor) {
+            //     // console.error('Options should be an object literal');
+            // }
+            // 
+                var accordion = document.querySelector(accordion);
+
+                getOptionsViaDataset(accordion);
+
+            // $A.options = options;
+            // $A.accordion = accordion;
+            // $A.id = randomReference() + uniqueID.substr(uniqueID.length - 5, uniqueID.length - 1);
+            // $A.setDefaults();
+            // $A.getElements();
+            // $A.setInitialState();
+            // $A.bindEvents(simpleAccordion.filterEvents, simpleAccordion.toggleSection, this);
+
+            // // console.info($A.id, $A.accordion)
+            // public.destroy = $A.destroy;
+            // return public;
+        }
+
+        simpleAccordion.setDefaults = function() {
+            this.defaults.event = this.options.event || 'click';
+            this.defaults.dimension = this.options.orientation === 'horizontal' ? 'width' : 'height';
+            this.defaults.contentOverflow = this.options.contentOverflow || 'hidden';
+            this.defaults.exposure = this.options.exposure || 0;
+            this.defaults.dynamicContent = !this.options.dynamicContent || true;
+        }
+
+        simpleAccordion.getElements = function() {
+            var el = this.el,
+                sectionNodes = this.sectionNodes,
+                options = this.options,
+                prefix = 'section-',
+                id;
+
+            sectionNodes = sectionNodes.slice.call(this.accordion.querySelectorAll(options.section));
+
+            sectionNodes.forEach(function(section, i) {
+                id = el[prefix + i] = {};
+                id.section = section;
+                id.switch = section.querySelector(options.switch);
+                id.indicator = section.querySelector(options.indicator);
+                id.content = section.querySelector(options.content);
+                id.contentBody = el[prefix + i].content.children[0];
+                id.close = el[prefix + i].contentBody.querySelector(options.close);
+            });
+        }
+
+        simpleAccordion.setInitialState = function() {
+            var el = this.el;
+            for (section in el) {
+                // Get contentBody element
+                var contentBody = el[section].contentBody;
+                // Hide contents overflow
+                el[section].content.style.overflow = this.defaults.contentOverflow;
+                // Set content sections to zero dimension
+                el[section].content.style[this.defaults.dimension] = this.defaults.exposure;
+                // Set content transition 
+                el[section].content.style[fix('transition')] = 'all 0.5s ease';
+                // Get computed dimension immediately if content body is not dynamic
+                if (this.defaults.dynamicContent) {
+                    this.store.contentComputedHeights[section] = parseInt(window.getComputedStyle(contentBody, null).getPropertyValue(this.defaults.dimension), 10);
+                }
+            }
+        }
+
+        simpleAccordion.filterEvents = function(e, toggleSection, $A) {
+            var el = $A.el,
+                target = e.target,
+                currentSection;
+            for (section in el) {
+                currentSection = el[section];
+                if (currentSection.switch === target) {
+                    toggleSection(currentSection, section, $A);
+                }
+            }
+        }
+
+        simpleAccordion.bindEvents = function(filterEvents, toggleSection, $A) {
+            this.accordion.addEventListener(this.defaults.event, function(e) {
+                filterEvents(e, toggleSection, $A);
+            }, false);
+        }
+
+        simpleAccordion.toggleSection = function(section, sectionName, $A) {
+            var contentBodyDimension = $A.store.contentComputedHeights[sectionName],
+                contentClosed = parseInt(window.getComputedStyle(section.content, null).getPropertyValue($A.defaults.dimension), 10);
+
+            if (contentClosed) {
+                section.content.style.height = 0;
+            } else {
+                // Get computed dimension if content is dynamic
+                if (!$A.defaults.dynamicContent) {
+                    $A.store.contentComputedHeights[sectionName] = parseInt(window.getComputedStyle(section.contentBody, null).getPropertyValue($A.defaults.dimension), 10);
+                }
+                section.content.style.height = contentBodyDimension + 'px';
+            }
+        }
+
+        simpleAccordion.destroy = function() {
+            console.log(simpleAccordion.accordion, simpleAccordion.defaults.event)
+            simpleAccordion.accordion.removeEventListener(simpleAccordion.defaults.event, simpleAccordion.filterEvents);
+        }
+
+        return simpleAccordion.init(accordion, options);
+    }
 }(window, document, undefined))
