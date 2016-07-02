0988a6b5a3ae582aba9e3c15835bce99c4b60b7f
Julien <Julienetie@gmail.com>
2016-07-02 11:30:20 +0100

    Removed spacing between wrappers

--- simple-accordion.js
+++ simple-accordion.js
@@ -1,60 +1,6 @@
-// var prefix = (function() {
-//     var styles = window.getComputedStyle(document.documentElement, ''),
-//         pre = (Array.prototype.slice
-//             .call(styles)
-//             .join('')
-//             .match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
-//         )[1],
-//         dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
-//     return {
-//         dom: dom,
-//         lowercase: pre,
-//         css: '-' + pre + '-',
-//         js: pre[0].toUpperCase() + pre.substr(1)
-//     };
-// })();
-
-
-// console.log(prefix);
-
-
-// var contents = [].slice.call(document.getElementsByClassName('content'));
-// var switches = [].slice.call(document.getElementsByClassName('switch'));
-
-// contents.forEach(function(content) {
-
-//     var content
-//         // content.style.display = 'none';
-//         // content.style.display = 'none';
-//     content.style.height = '0';
-//     // content.style.position = 'absolute';
-//     content.style.overflow = 'hidden';
-//     content.style.transition = 'all 0.5s ease';
-// });
-
-// switches.forEach(function(switch_) {
-//     switch_.addEventListener('click', function() {
-//         var content = this.parentNode.children[1];
-//         var contentHeight = parseInt(window.getComputedStyle(content, null).getPropertyValue("height"), 10);
-//         console.log(contentHeight)
-//         if (contentHeight) {
-//             this.parentNode.children[1].style.height = 0;
-//         } else {
-//             var contentBody = this.parentNode.children[1].children[0];
-//             var contentBodyHeigtht = window.getComputedStyle(contentBody, null).getPropertyValue("height");
-//             this.parentNode.children[1].style.height = contentBodyHeigtht;
-//         }
-//     })
-
-// });
-
 (function (window) {
 /////////////////////////
-
-
-
-
-
+/////////////////////////
 var simpleAccordion = function(accordion, options) {
         var simpleAccordion = {},
             $A = simpleAccordion;
@@ -244,13 +190,7 @@ var simpleAccordion = function(accordion, options) {
 
         return simpleAccordion.init(accordion, options);
     };
-
-
-
-
-
-
-
+//////////////////////////////////////
 //////////////////////////////////////
 
 // Node.js/ CommonJS
